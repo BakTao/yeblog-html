c25a753def2d32b37a64005e47d2a25ee9fedb0f
叶百韬 <1363052695@qq.com>
2020-03-09 23:46:21 +0800

    注册量统计完成

--- assets/js/charts/registerChart.js
+++ assets/js/charts/registerChart.js
@@ -1,17 +1,54 @@
+layui.use(['form'], function () {
+    changeRegChart()
+});
+
 var registerChart = echarts.init(document.getElementById('register-chart'));
 
-option = {
-    xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-        type: 'value'
-    },
-    series: [{
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-        smooth: true
-    }]
-};
-registerChart.setOption(option);
+function changeRegChart(){
+    var swjd = $("#sjwd").val();
+    if(swjd = "1"){
+        $(".card-header h4").text("近5年的注册量统计")
+    }else if(swjd = "2"){
+        $(".card-header h4").text("近6月的注册量统计")
+    }else{
+        $(".card-header h4").text("近15天的注册量统计")
+    }
+
+    $.ajax({
+        url: "",
+        type: "post",
+        data: JSON.stringify({"swjd":swjd}),
+        success: function(data){
+            initRegChart(data.body);
+        }
+    })
+}
+
+function initRegChart(data){
+    var xData = [];
+    var yData = [];
+
+    for(var i = 0; i < data.length; i++){
+        xData.push(data.tjny);
+        yData.push(data.value);
+    }
+
+    option = {
+        tooltip: {
+            trigger: 'item'     //提示
+        },
+        xAxis: {
+            type: 'category',
+            data: xData
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: yData,
+            type: 'line',
+            smooth: true
+        }]
+    };
+    registerChart.setOption(option);
+}
